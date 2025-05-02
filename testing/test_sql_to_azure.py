dfe39a2d458caf0637f1777ee330c4186ae7c865
Nikki-Jain17 <jainnikki729@gmail.com>
2025-05-02 14:33:11 +0530

    Github Actions yaml file changes-datafabric portal implementation

--- testing/test_sql_to_azure.py
+++ testing/test_sql_to_azure.py
@@ -6,12 +6,7 @@ from config_loader import load_config  # wherever your load_config is
 @allure.suite("SQL to Azure Suite")
 @allure.feature("SQL to Azure Workflow")
 @pytest.mark.P1
-def test_sql_to_azure(flow="sql_to_azure", component=None, request=None):
-    if request:
-        flow_from_cli = request.config.getoption("--flow")
-        component_from_cli = request.config.getoption("--component")
-        flow = flow_from_cli if flow_from_cli else flow
-        component = component_from_cli if component_from_cli else component
+def test_sql_to_azure(flow, component):
 
     with allure.step(f"Running flow: {flow}"):
         print(f"Running flow: {flow}")
@@ -21,7 +16,7 @@ def test_sql_to_azure(flow="sql_to_azure", component=None, request=None):
 
     e2e_flow_config = load_config(f'{flow}/e2eflow.yml')
 
-    if component is None or component == "none":
+    if component is None or component.tolower() == "none":
         # End-to-end mode: Run **all components** one by one
         for comp in e2e_flow_config:
             with allure.step(f"Running full E2E flow for component: {comp}"):
