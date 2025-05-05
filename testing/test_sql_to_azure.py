567d33a5cbd62ba28ef928db90d8a5d1257ff309
Nikki-Jain17 <jainnikki729@gmail.com>
2025-05-05 14:28:56 +0530

    Resolving component execution-datafabric portal implementation

--- testing/test_sql_to_azure.py
+++ testing/test_sql_to_azure.py
@@ -1,18 +1,12 @@
 import pytest
 import allure
 from config_loader import load_config  # wherever your load_config is
-from _pytest.fixtures import FixtureRequest
 
-def run_component_logic(flow, component):
-    print(f"Executing real logic for {component} inside {flow} flow")
-    # Place actual call here
 
 @allure.suite("SQL to Azure Suite")
 @allure.feature("SQL to Azure Workflow")
 @pytest.mark.P1
-def test_sql_to_azure(request: FixtureRequest):
-    flow = request.config.getoption("--flow")
-    component = request.config.getoption("--component")
+def test_sql_to_azure(flow, component):
 
     with allure.step(f"Running flow: {flow}"):
         print(f"Running flow: {flow}")
@@ -22,22 +16,17 @@ def test_sql_to_azure(request: FixtureRequest):
 
     e2e_flow_config = load_config(f'{flow}/e2eflow.yml')
 
-    if not component or component.lower() == "none":
+    if component is None or component == "none":
+        # End-to-end mode: Run **all components** one by one
         for comp in e2e_flow_config:
-            if e2e_flow_config[comp]["enabled"]:
-                with allure.step(f"Running full E2E flow for component: {comp}"):
-                    print(f"Executing full E2E flow logic for component: {comp}")
-                    run_component_logic(flow, comp)
-            else:
-                print(f"Skipping disabled component: {comp}")
+            with allure.step(f"Running full E2E flow for component: {comp}"):
+                print(f"Executing full flow logic for component: {comp}")
+                # you can call your function to run component here
     else:
         if component not in e2e_flow_config:
             pytest.fail(f"Component '{component}' not found in {flow}/e2eflow.yml")
 
         if e2e_flow_config[component]["enabled"]:
-            print(f"Exact Running component: {component}")
-            run_component_logic(flow, component)
-
             with allure.step(f"{component} execution successful for {flow}"):
                 print(f"{component} execution successful for {flow}")
         else:
