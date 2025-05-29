1022e6dc253943af63245e3f86fc346cb28b9505
Nikki-Jain17 <jainnikki729@gmail.com>
2025-05-29 10:56:09 +0530

    github actions testing

--- testing/test_google.py
+++ testing/test_google.py
@@ -4,16 +4,23 @@ from selenium.webdriver.common.by import By
 import time
 
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 
 def test_google_search():
-    options = webdriver.ChromeOptions()
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('--headless')  # Run in headless mode
+    # options.add_argument('--no-sandbox')
+    # options.add_argument('--disable-dev-shm-usage')
+    #
+    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+    options = webdriver.EdgeOptions()
     options.add_argument('--headless')  # Run in headless mode
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
-    # driver = webdriver.Edge()
+    driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()), options=options)
+
     driver.get("https://www.google.com")
 
     search_box = driver.find_element(By.NAME, "q")
